d70fd7146a934e29698135db4ac3472f16d36dea
Benjamin Ratti <ben@ratti.fr>
2023-05-16 21:09:49 +0200

    feat(api): init api project

---
+++ api/src/main/java/com/benratti/openpp/api/OpenPpApiApplication.java
@@ -0,0 +1,13 @@
+package com.benratti.openpp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenPpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenPpApiApplication.class, args);
+	}
+
+}
