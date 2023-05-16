b9644f1263ff790c8cc8c0f2b145caddf7964003
Benjamin Ratti <ben@ratti.fr>
2023-05-16 21:09:49 +0200

    feat(api): init api project

---
+++ api/src/test/java/com/benratti/openpp/api/OpenPpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benratti.openpp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenPpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
